30a9edb5903f721e5727878d39ef8cc1ade8d6cf
Tuukka Järviluoma <tuukka.jarviluoma@gmail.com>
2024-11-04 18:09:24 +0200

    Done

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,9 +1,10 @@
-//import React from 'react';
-import styled from 'styled-components';
-
+import React from 'react';
+import styles from './Button.module.css';
+//import styled from 'styled-components';
 //import './Button.css';
 
-const Button = styled.button`
+/*const Button = styled.button`
+    width: 100%;
     font: inherit;
     padding: 0.5rem 1.5rem;
     border: 1px solid #8b005d;
@@ -12,6 +13,9 @@ const Button = styled.button`
     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
     cursor:pointer;
   
+    @media (min-width: 768px) {
+      width: auto;
+    }
 
   &:focus {
     outline:none;
@@ -23,15 +27,15 @@ const Button = styled.button`
     border-color: #ac0e77;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
-`;
+`;*/
 
-/*const Button = props => {
+const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
 };
-*/
+
 
 export default Button;
